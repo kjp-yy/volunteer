4e08d5057a25ca1deb25b4a82f97ef8c56b6f9a3
kjp-yy <1129861050@qq.com>
2020-06-04 14:59:55 +0800

    6-4_1

--- pages/index1/index1.js
+++ pages/index1/index1.js
@@ -18,9 +18,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // res.SEND('volunteer/activityList','GET',{},(res)=>{
-    //   // console.log(res)
-    // })
+    res.SEND('website/canjiaList','GET',{},(res)=>{
+      console.log(res)
+    })
   },
 
   /**
@@ -29,46 +29,15 @@ Page({
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
+  },
+  click(){
+    wx.navigateTo({
+      url: '../haa_look/haa_look',
+    })
   }
 })
